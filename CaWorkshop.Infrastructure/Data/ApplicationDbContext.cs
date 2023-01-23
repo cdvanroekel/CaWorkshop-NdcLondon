403ad4d677f6502d56dfc1830e415b417cbcd91f
cdvanroekel <cdvanroekel@gmail.com>
2023-01-23 16:28:49 +0000

    Adds Mediatr and CQRS principles

--- CaWorkshop.Infrastructure/Data/ApplicationDbContext.cs
+++ CaWorkshop.Infrastructure/Data/ApplicationDbContext.cs
@@ -5,10 +5,11 @@ using Duende.IdentityServer.EntityFramework.Options;
 using CaWorkshop.Domain.Entities;
 using System.Reflection;
 using CaWorkshop.Infrastructure.Identity;
+using CaWorkshop.Application.Common.Interfaces;
 
 namespace CaWorkshop.Infrastructure.Data;
 
-public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
         : base(options, operationalStoreOptions)
